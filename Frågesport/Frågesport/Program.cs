ee4430c1bd7c5f8b680ce59487b28641ee927eea
Haleballe <oskarmorell1998@hotmail.se>
2018-11-22 13:00:54 +0100

    projekt

---
+++ Frågesport/Frågesport/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Frågesport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
