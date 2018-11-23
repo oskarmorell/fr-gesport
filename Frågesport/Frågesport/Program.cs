9bde2ace98285265ece20e58cc161d47721ddc79
Haleballe <oskarmorell1998@hotmail.se>
2018-11-23 11:05:55 +0100

    f

--- Frågesport/Frågesport/Program.cs
+++ Frågesport/Frågesport/Program.cs
@@ -10,15 +10,16 @@ namespace Frågesport
         //Card currentcard;
         static void Main(string[] args)
         {
+            //currentcard = new Card();
             Console.WriteLine("Första frågan är" /*+ Database.getquestion();*/);
             string answer = Console.ReadLine();
             //Card.iscorrect(answer);
             Console.WriteLine("Andra frågan är" /*+ Database.getquestion();*/);
             string answer2 = Console.ReadLine();
-            //Card.iscorrect(answer);
+            //Card.iscorrect(answer2);
             Console.WriteLine("Tredje frågan är" /*+ Database.getquestion();*/);
             string answer3 = Console.ReadLine();
-            //Card.iscorrect(answer);
+            //Card.iscorrect(answer3);
             Console.ReadKey();
         }
     }
