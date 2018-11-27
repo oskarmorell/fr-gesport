f9e17404a60ff6680091107ab0e9c6b27d90648d
Isacsven <isac.svensson99@gmail.com>
2018-11-27 13:24:57 +0100

    a

---
+++ Frågesport/Frågesport/Card.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Frågesport
+{
+    class Card
+    {
+        private string question;
+        private string answer;
+        
+        public Card(string fråga, string svar)
+        {
+            this.question = fråga;
+            this.answer = svar;
+        }
+        public string getquestion()
+        {
+            return question; 
+        }
+        public string getanswer()
+        {
+            return answer;
+        }
+        public bool iscorrect (string guess)
+        {
+            if (guess == answer)
+            {
+                return true;
+            }
+            if (guess != answer)
+            {
+                return false;
+            }
+        }
+    }
+}
