e95e68f3bd617e5e4725249120de10cd9a2be830
alex-range <alexander.range@yahoo.com>
2018-11-27 12:42:38 +0100

    skrev på databasen

---
+++ Frågesport/Frågesport/Database.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Frågesport
+{
+    class Database
+    {
+        private List<Card> qcards = new List<Card>();
+
+        public Database()
+        {
+        }
+
+        public string getquestion(Card qcard)
+        {
+            qcards.Add(qcard);
+        }
+
+    }
+}
